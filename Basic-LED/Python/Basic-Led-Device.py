f51d5a8db9661bdf4337a9f13765a1da746520b2
AdamMiltonBarker <adammiltonbarker@gmail.com>
2018-04-22 15:27:41 +0200

    [Basic-LED/Python] Code upgrade

--- Basic-LED/Python/Basic-Led-Device.py
+++ Basic-LED/Python/Basic-Led-Device.py
@@ -1,86 +1,80 @@
-# *****************************************************************************
-# Copyright (c) 2016 TechBubble Technologies and other Contributors.
-#
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the Eclipse Public License v1.0
-# which accompanies this distribution, and is available at
-# http://www.eclipse.org/legal/epl-v10.html 
-#
-# Contributors:
-#   Adam Milton-Barker - TechBubble Technologies Limited
-# *****************************************************************************
+############################################################################################
+# Title: IoT JumpWay Raspberry Pi Basic LED
+# Description: IoT connected LED using Raspberry Pi.
+# Last Modified: 2018-04-22
+############################################################################################
 
 import time
 import sys
 import json
 
 import RPi.GPIO as GPIO
-import techbubbleiotjumpwaymqtt.device
+import JumpWayMQTT.Device as JWMQTTdevice
 
 class BasicLED():
-    
+
     def __init__(self):
-        
+
         self.JumpWayMQTTClient = ""
         self.configs = {}
-        
+
         with open('config.json') as configs:
             self.configs = json.loads(configs.read())
-            
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(
             self.configs["Actuators"]["LED"]["PIN"],
             GPIO.OUT
         )
-        
+
         self.startMQTT()
-        
+
     def deviceCommandsCallback(self,topic,payload):
-        
+
         print("Received command data: %s" % (payload))
-        
+
         jsonData = json.loads(payload.decode("utf-8"))
-        
+
         if jsonData['ActuatorID']==self.configs["Actuators"]["LED"]["ID"] and jsonData['Command']=='TOGGLE' and jsonData['CommandValue']=='ON':
-            
+
             GPIO.output(
                 self.configs["Actuators"]["LED"]["PIN"],
                 GPIO.HIGH
             )
 
         elif jsonData['ActuatorID']==self.configs["Actuators"]["LED"]["ID"] and jsonData['Command']=='TOGGLE' and jsonData['CommandValue']=='OFF':
-            
+
             GPIO.output(
                 self.configs["Actuators"]["LED"]["PIN"],
                 GPIO.LOW
             )
-            
+
     def startMQTT(self):
-        
+
         try:
-            
-            self.JumpWayMQTTClient = techbubbleiotjumpwaymqtt.device.JumpWayPythonMQTTDeviceConnection({
-				"locationID": self.configs["IoTJumpWaySettings"]["SystemLocation"],  
-				"zoneID": self.configs["IoTJumpWaySettings"]["SystemZone"], 
-				"deviceId": self.configs["IoTJumpWaySettings"]["SystemDeviceID"], 
-				"deviceName": self.configs["IoTJumpWaySettings"]["SystemDeviceName"], 
-				"username": self.configs["IoTJumpWayMQTTSettings"]["username"], 
-				"password": self.configs["IoTJumpWayMQTTSettings"]["password"]
-			})
-            
+
+            self.jumpwayClient = JWMQTTdevice.DeviceConnection({
+                "locationID": self.configs["IoTJumpWay"]["Location"],
+                "zoneID": self.configs["IoTJumpWay"]["Zone"],
+                "deviceId": self.configs["IoTJumpWay"]["Device"],
+                "deviceName": self.configs["IoTJumpWay"]["DeviceName"],
+                "username": self.configs["IoTJumpWayMQTT"]["MQTTUsername"],
+                "password": self.configs["IoTJumpWayMQTT"]["MQTTPassword"]
+            })
+
         except Exception as e:
             print(str(e))
             sys.exit()
-            
-        self.JumpWayMQTTClient.connectToDevice()
-        self.JumpWayMQTTClient.subscribeToDeviceChannel("Commands")
-        self.JumpWayMQTTClient.deviceCommandsCallback = self.deviceCommandsCallback
-        
+
+        self.jumpwayClient.connectToDevice()
+        self.jumpwayClient.subscribeToDeviceChannel("Commands")
+        self.jumpwayClient.deviceCommandsCallback = self.deviceCommandsCallback
+
 BasicLED = BasicLED()
 
 while True:
-    
+
     pass
-    
-BasicLED.JumpWayMQTTClient.disconnectFromDevice()
+
+BasicLED.jumpwayClient.disconnectFromDevice()
